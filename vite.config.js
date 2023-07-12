ab0dc8f722e612e1db2c751bd00063d84bb9159d
AshleyWrightA <AshleyWrightAppDev@Outlook.com>
2023-07-12 14:30:48 -0600

    .

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+export default {
+  // Base public path when served in production
+  base: "/cv-site-revamp/",
+
+  // Build output configuration
+  build: {
+    outDir: "dist", // Specify the output directory for the production build
+  },
+
+  // Configure the plugins
+  plugins: [],
+
+  // Configure the CSS preprocessor
+  css: {
+    preprocessorOptions: {
+      // Configure the options for the preprocessor
+    },
+  },
+
+  // Additional configuration options...
+};
